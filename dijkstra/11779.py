7909e82d099a18cbbf0c29cd3d2b9c0ecfd73168
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-11 01:33:14 +0900

    411_1

--- dijkstra/11779.py
+++ dijkstra/11779.py
@@ -15,23 +15,41 @@ for i in range(M):
     U, W, V = map(int, input().split())
     graph[U].append((W, V))
 
+for i in graph:
+    print(i, end="\n")
+
 def dijkstra(start, end):
     queue = []
     heapq.heappush(queue, (start, 0))
     distance[start] = 0
+    route = []
+    route.append(start)
 
     while (queue):
         now, dist = heapq.heappop(queue)
+        print(now, dist)
+        save_route = ""
+        flag = 0
         if distance[now] < dist:
             continue
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
+                # print(i[1], cost)
                 distance[i[0]] = cost
                 heapq.heappush(queue, (i[0], cost))
+                print(queue)
+                print(distance)
+                save_route = i[0]
+                flag = 1
+        if flag == 1:
+            route.append(save_route)
+
+    return distance[end], route
 
 
 start, end = map(int, input().split())
-dijkstra(start, end)
-
-print(distance[end])
+ans, route = dijkstra(start, end)
+print(ans)
+print(len(route))
+print(route)
