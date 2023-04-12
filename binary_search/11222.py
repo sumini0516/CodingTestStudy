84d1affee72a7f568b47bef405057f77142e3a01
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-12 23:25:20 +0900

    412_5_2805 review

---
+++ binary_search/11222.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start, end = 1, max(trees)
+
+while (start <= end):
+    mid = (start + end) // 2
+    cut_tree = 0
+    for i in trees:
+        if i > mid:
+            cut_tree += i - mid
+
+    if cut_tree >= M:
+        start = mid + 1
+
+    else:
+        end = mid - 1
+
+print(end)
