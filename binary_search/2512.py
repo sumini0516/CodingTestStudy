f5e84b46bffe5a77356ea7f30ea38ad19d509cb4
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-13 00:54:42 +0900

    413_2

---
+++ binary_search/2512.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+region = list(map(int, input().split()))
+M = int(input().rstrip())
+
+start = 1
+end = max(region)
+region.sort()
+
+while (start <= end):
+    mid = (start + end) // 2
+    budget = 0
+
+    for money in region:
+        if money > mid:
+            budget += mid
+        else:
+            budget += money
+
+    if budget <= M:
+        start = mid + 1
+
+    else:
+        end = mid - 1
+
+print(end)
