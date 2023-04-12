bdd6c95d51a0329426b5da91e40ad333f29eb060
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-13 00:23:44 +0900

    413_1

---
+++ binary_search/입국심사.py
@@ -0,0 +1,22 @@
+def solution(n, times):
+    answer = 0
+    times.sort()
+
+    start = 1
+    end = max(times) * n
+
+    while start <= end:
+        people = 0
+        mid = (start + end) // 2
+
+        for time in times:
+            people += (mid // time)
+
+        if people >= n:
+            answer = mid
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    return answer
