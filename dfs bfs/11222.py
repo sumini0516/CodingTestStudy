4e69ffa81cdcec69e761a249b0a73ac866933a0d
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-12 19:00:09 +0900

    412_3

--- dfs bfs/11222.py
+++ dfs bfs/11222.py
@@ -1,14 +1,15 @@
 import copy
 
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dy = [0, 0, 1, -1]
 
 
 def dfs(i, j, graph, position, n, num):
     res = [position]
 
     for k in range(4):
-        nx, ny = i + dx[k], j + dy[k]
+        nx = dx[k] + i
+        ny = dy[k] + j
 
         if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == num:
             graph[nx][ny] = 2
@@ -20,14 +21,13 @@ def dfs(i, j, graph, position, n, num):
 def rotate(lst):
     n = len(lst)
 
-    #회전한 테이블
-    o = [[0] * n for _ in range(n)]
+    new_table = [[0] * n for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
-            o[j][n - 1 - i] = lst[i][j]
+            new_table[j][n - 1 - i] = lst[i][j]
 
-    return o
+    return new_table
 
 
 def solution(game_board, table):
@@ -36,38 +36,39 @@ def solution(game_board, table):
     game_board_copy = copy.deepcopy(game_board)
     block = []
 
-
-    # 블록에 있는 빈 칸 저장
+    # game_board에 있는 빈 칸의 모양 저장
     for i in range(n):
         for j in range(n):
+            # game_board가 빈칸이면 dfs로 빈 곳 마저 탐색
             if game_board_copy[i][j] == 0:
                 game_board_copy[i][j] = 2
                 result = dfs(i, j, game_board_copy, [0, 0], n, 0)[1:]
-                print(result)
                 block.append(result)
+
+    print(block)
 
     for r in range(4):
         table = rotate(table)
-        table_rotate_copy = copy.deepcopy(table)
+        table_copy = copy.deepcopy(table)
 
         for i in range(n):
             for j in range(n):
-                if table_rotate_copy[i][j] == 1:
-                    table_rotate_copy[i][j] = 2
-                    result = dfs(i, j, table_rotate_copy, [0, 0], n, 1)[1:]
+                if table_copy[i][j] == 1:
+                    table_copy[i][j] = 2
+                    result = dfs(i, j, table_copy, [0, 0], n, 1)[1:]
                     if result in block:
+                        answer += len(result) + 1
                         block.pop(block.index(result))
-                        answer += (len(result) + 1)
-                        table = copy.deepcopy(table_rotate_copy)
+                        table = copy.deepcopy(table_copy)
                     else:
-                        table_rotate_copy = copy.deepcopy(table)
+                        table_copy = copy.deepcopy(table)
 
     return answer
 
 
 ans = solution([[1, 1, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0], [0, 1, 1, 0, 0, 1], [1, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0],
-          [0, 1, 1, 1, 0, 0]],
-         [[1, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 1, 0],
-          [0, 1, 0, 0, 0, 0]])
+                [0, 1, 1, 1, 0, 0]],
+               [[1, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 1, 0],
+                [0, 1, 0, 0, 0, 0]])
 
 print(ans)
