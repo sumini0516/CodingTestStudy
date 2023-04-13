538faaff9c591b2595dfcc6e7661348910527a8d
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-04-13 14:03:50 +0900

    413_3

--- binary_search/1939.py
+++ binary_search/1939.py
@@ -1,6 +1,54 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+
+def bfs(weight):
+    queue = deque()
+    queue.append(A)
+    visited = [0] * (N + 1)
+    visited[A] = 1
+
+    while (queue):
+        V = queue.popleft()
+
+        for i, w in graph[V][1:]:
+            if visited[i] == 0 and w >= weight:
+                visited[i] = 1
+                queue.append(i)
+
+    if visited[B]:
+        return True
+    else:
+        return False
+
+
 N, M = map(int, input().split())
 
+graph = [[0] for _ in range(N + 1)]
+# print(graph)
+
+for i in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+    graph[b].append([a, c])
+
+# print(graph)
+
+A, B = map(int, input().split())
+
+start = 0
+end = 1000000000
+
+while (start <= end):
+    mid = (start + end) // 2
+
+    if bfs(mid):
+        result = mid
+        start = mid + 1
+
+    else:
+        end = mid - 1
+
+print(result)
