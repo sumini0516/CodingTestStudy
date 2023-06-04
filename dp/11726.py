8376bf610ce46278aa534adf109b90a2ad65338e
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-06-04 19:58:55 +0900

    CodingTestStudy_604_1

---
+++ dp/11726.py
@@ -0,0 +1,16 @@
+# dp[i] = dp[i - 1] + dp[i - 2]
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * (N + 1)
+dp[1] = 1
+dp[2] = 2
+
+if N >= 3:
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[N] % 10007)
