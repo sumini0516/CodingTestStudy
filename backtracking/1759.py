b3101f389519fa1e962d19e437bf2b087928bf32
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-06-05 18:38:48 +0900

    CodingTestStudy_605_1

---
+++ backtracking/1759.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 3
+
+if N >= 3:
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] * 2
+    print(dp[N] % 10007)
+else:
+    print(dp[N])
--- dp/11726.py
+++ dp/11726.py
@@ -6,11 +6,13 @@ input = sys.stdin.readline
 
 N = int(input())
 dp = [0] * (N + 1)
-dp[1] = 1
-dp[2] = 2
 
 if N >= 3:
+    dp[1] = 1
+    dp[2] = 2
     for i in range(3, N + 1):
         dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[N] % 10007)
 
-print(dp[N] % 10007)
+else:
+    print(N)
