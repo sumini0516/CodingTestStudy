3480bac17a10ca721616f7f3c65699e3669e6ec9
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-06-28 22:22:14 +0900

    CodingTestStudy_628_3

--- dp/11660.py
+++ dp/11660.py
@@ -14,12 +14,8 @@ for i in range(N):
 for i in range(1, N + 1):
     for j in range(1, N + 1):
         dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + graph[i - 1][j - 1]
-        print("dp",i, ":", j, dp[i][j])
 
-print(dp)
 for j in range(M):
-    x1, y1, x2, y2 = map(int, input().rstrip().split(" "))
-    # if x1 == x2 and y1 == y2:
-    #     answer = graph[x1][y1]
-    #     break
-    # else:
+    x1, y1, x2, y2 = map(int, input().split(" "))
+    answer = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]
+    print(answer)
