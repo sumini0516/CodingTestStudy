d4e8b93123df13afdb9445de7f97bce93402d3b0
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-07-03 23:10:39 +0900

    CodingTestStudy_703_1

--- dfs bfs/14940.py
+++ dfs bfs/14940.py
@@ -3,42 +3,50 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def bfs(start):
+def bfs(x, y):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, 1, -1]
 
     queue = deque()
-    queue.append((start[0], start[1]))
-    visited[start[0]][start[1]] = 0
+    queue.append((x, y))
+    visited[x][y] = 1
+    res[x][y] = 0
 
     while queue:
         x, y = queue.popleft()
-        print(x, y)
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if graph[nx][ny] == 1 and 0 <= nx < M and 0 <= ny < N and visited[nx][ny] == -1:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx, ny))
+            if 0 <= nx < M and 0 <= ny < N and visited[nx][ny] == 0:
+                if graph[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    res[nx][ny] = 0
+                else:
+                    visited[nx][ny] = 1
+                    res[nx][ny] = res[x][y] + 1
+                    queue.append((nx, ny))
     return
-
 
 
 N, M = map(int, input().split(" "))
 graph = list()
-visited = [[-1] * M for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+res = [[-1] * M for _ in range(N)]
 
 for i in range(N):
     A = list(map(int, input().rstrip().split(" ")))
     graph.append(A)
     if 2 in A:
-        start = (i, A.index(2))
+        start_x = i
+        start_y = A.index(2)
+
+bfs(start_x, start_y)
 
 for i in range(N):
     for j in range(M):
-        if visited[i][j] == -1:
+        if graph[i][j] == 0:
             print(0, end = " ")
         else:
-            print(visited[i][j], end = " ")
-    print("\n")
+            print(res[i][j], end = " ")
+    print()
 
