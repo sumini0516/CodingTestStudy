714e8fa044bd1fc6d46b0be02d5b1f7222f3a5f0
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-07-07 16:44:33 +0900

    CodingTestStudy_707_1

--- dp/15486.py
+++ dp/15486.py
@@ -4,9 +4,19 @@ input = sys.stdin.readline
 
 N = int(input())
 dp = [0] * (N + 1)
-TP = list()
+T = [0] * (N + 1)
+P = [0] * (N + 1)
 
-for _ in range(N):
-    T, P = map(int, input().split(" "))
-    TP.append((T, P))
+for i in range(N):
+    t, p = map(int, input().split(" "))
+    T[i + 1] = t
+    P[i + 1] = p
+
+for i in range(1, N + 1):
+    dp[i] = max(dp[i], dp[i - 1])
+    complete = i + T[i] - 1
+    if complete <= N:
+        dp[complete] = max(dp[complete], dp[i - 1] + P[i])
+
+print(max(dp))
 
