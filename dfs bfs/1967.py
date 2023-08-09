15bd0cb2d9df5a28d6039b35c1462874a9bbcafa
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-08-09 22:33:13 +0900

    CodingTestStudy_809_2

--- dfs bfs/1967.py
+++ dfs bfs/1967.py
@@ -1,13 +1,31 @@
-import queue
 import sys
 from collections import defaultdict
 
-
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 graph = defaultdict(list)
 
-N = int(input())
-for i in range(N):
-    u, w, v = map(int, input().split(" "))
-    graph[u].append((w, v))
+def bfs(node, dist):
+    for next_node, next_dist in graph[node]:
+        if visited[next_node] == -1:
+            visited[next_node] = dist + next_dist
+            bfs(next_node, dist + next_dist)
 
+    return max(visited)
+
+N = int(input())
+for i in range(N - 1):
+    u, w, v = map(int, input().rstrip().split(" "))
+    graph[u].append((w, v))
+    graph[w].append((u, v))
+
+visited = [-1] * (N + 1)
+visited[1] = 0
+answer = bfs(1, 0)
+A = visited.index(answer)
+
+visited = [-1] * ( N + 1)
+visited[A] = 0
+answer = bfs(A, 0)
+
+print(answer)
