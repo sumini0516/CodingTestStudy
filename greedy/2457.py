94d8ec150fa09d6c3c6d3f6b1f1b950bb9469f88
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-08-15 23:11:38 +0900

    CodingTestStudy_815_1

---
+++ greedy/2457.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+flowers = list()
+start_month = 3
+start_day = 1
+end_month = 11
+end_day = 30
+
+for i in range(N):
+    SM, SD, EM, ED = map(int, input().split())
+    flowers.append((SM, SD, EM, ED))
+
