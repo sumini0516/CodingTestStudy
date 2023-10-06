1d203c211ee159fa80ffd1af0ad0ec3555eba82a
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-06 20:09:02 +0900

    CodingTestStudy_1006_1

--- dfs bfs/review.py
+++ dfs bfs/review.py
@@ -1,30 +1,25 @@
-import sys
-from collections import defaultdict
+from collections import deque
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
-graph = defaultdict(list)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def bfs(node, dist):
-    for next_node, next_dist in graph[node]:
-        if visited[next_node] == -1:
-            visited[next_node] = dist + next_dist
-            bfs(next_node, dist + next_dist)
+def bfs(start_x, start_y):
+    queue = deque()
+    queue.append((start_x, start_y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    print(graph)
+    return graph[N - 1][M - 1]
 
-    return visited.index(max(visited))
+N, M = map(int, input().split())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().rstrip())))
 
-N = int(input())
-for i in range(N - 1):
-    u, w, v = map(int, input().rstrip().split(" "))
-    graph[u].append((w, v))
-    graph[w].append((u, v))
-
-visited = [-1] * (N + 1)
-visited[1] = 0
-a = bfs(1, 0)
-print(visited)
-print(a)
-visited = [-1] * (N + 1)
-visited[a] = 0
-answer = bfs(a, 0)
-print(max(visited))
+print(bfs(0, 0))
