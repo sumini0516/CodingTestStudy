7de43d9306c424382724b57cf210f43c5deca11c
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-19 14:40:19 +0900

    CodingTestStudy_1019_1

---
+++ data structure/2776.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    ans = list()
+    N = int(input())
+    see = list(map(int, input().split(" ")))
+    see.sort()
+    M = int(input())
+    find = list(map(int, input().split(" ")))
+
+    for i in find:
+        start = 0
+        end = len(see) - 1
+        answer = 0
+        while start <= end:
+            mid = (start + end) // 2
+            if see[mid] == i:
+                answer = 1
+                break
+            elif see[mid] < i:
+                start = mid + 1
+            else:
+                end = mid - 1
+        ans.append(answer)
+
+    for i in ans:
+        print(i)
--- etc/review.py
+++ etc/review.py
@@ -1,32 +1,50 @@
 import sys
-from collections import defaultdict
-import heapq
+from collections import deque
+import copy
 
 input = sys.stdin.readline
-INF = int(1e9)
 
-def dijkstra(start, end):
-    distance = [INF] * (N + 1)
-    queue = list()
-    heapq.heappush(queue, (0, start))
-    distance[start] = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    queue = deque()
+    tmp_graph = copy.deepcopy(graph)
+    for i in range(N):
+        for j in range(M):
+            if tmp_graph[i][j] == 2:
+                queue.append((i, j))
     while queue:
-        cost, node = heapq.heappop(queue)
-        if distance[node] < cost:
-            continue
-        for next_cost, next_node in graph[node]:
-            compare = distance[node] + next_cost
-            if compare < graph[next_node]:
-                distance[node] = compare
-                heapq.heappush(queue, (compare, next_node))
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0 <= nx < N and 0 <= ny < M:
+                if tmp_graph[nx][ny] == 0:
+                    tmp_graph[nx][ny] = 2
+                    queue.append((nx, ny))
 
+    global answer
+    ans = 0
+    for i in range(N):
+        ans += graph[i].count(0)
+    answer = max(answer, ans)
 
-N, M = map(int, input().split(" "))
-graph = defaultdict(list)
-flag = 1
-for i in range(M):
-    a, b, c = map(int, input().split(" "))
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-V1, V2 = map(int, input().split(" "))
+def makewall(cnt):
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 2:
+                graph[i][j] = 1
+                makewall(cnt + 1)
+                graph[i][j] = 2
 
+N, M = map(int, input().split())
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input().split())))
+answer = 0
+makewall(0)
+print(answer)
