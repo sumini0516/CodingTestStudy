336bdd4174baf568177735d7d21e3f13be372753
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-21 13:22:47 +0900

    CodingTestStudy_1021_7

--- dp/review.py
+++ dp/review.py
@@ -3,21 +3,20 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-stairs = list()
 dp = [0] * N
+drink = [0] * N
 
 for i in range(N):
-    stairs.append(int(input()))
-
-dp[0] = stairs[0]
-
+    M = int(input())
+    drink[i] = M
+print(drink)
+dp[0] = drink[0]
 if N > 1:
-    dp[1] = stairs[0] + stairs[1]
-
+    dp[1] = drink[0] + drink[1]
 if N > 2:
-    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+    dp[2] = max(dp[1], drink[0] + drink[2], drink[1] + drink[2])
 
 for i in range(3, N):
-    dp[i] = max(stairs[i] + dp[i - 2], dp[i - 3] + stairs[i] + stairs[i - 1])
+    dp[i] = max(dp[i - 1], drink[i] + dp[i - 3] + drink[i - 1], drink[i] + dp[i - 2])
 
 print(dp[-1])
