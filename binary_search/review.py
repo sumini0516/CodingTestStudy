9ae3785eb92f3b1c4af8b45381bdfd9cb6adee8f
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-21 13:10:53 +0900

    CodingTestStudy_1021_6

--- binary_search/review.py
+++ binary_search/review.py
@@ -1,54 +1,24 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-
-def bfs(weight):
-    queue = deque()
-    queue.append(A)
-    visited = [0] * (N + 1)
-    visited[A] = 1
-
-    while (queue):
-        V = queue.popleft()
-
-        for i, w in graph[V][1:]:
-            if visited[i] == 0 and w >= weight:
-                visited[i] = 1
-                queue.append(i)
-
-    if visited[B]:
-        return True
-    else:
-        return False
-
-
-N, M = map(int, input().split())
-
-graph = [[0] for _ in range(N + 1)]
-# print(graph)
-
-for i in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append([b, c])
-    graph[b].append([a, c])
-
-# print(graph)
-
-A, B = map(int, input().split())
-
+N = int(input())
+liquid = list(map(int, input().split(" ")))
+liquid.sort()
 start = 0
-end = 1000000000
+end = len(liquid) - 1
+answer = [liquid[start], liquid[end]]
+compare = abs(liquid[start] + liquid[end])
 
-while (start <= end):
+while(start < end):
     mid = (start + end) // 2
-
-    if bfs(mid):
-        result = mid
+    compare2 = liquid[start] + liquid[end]
+    if abs(compare2) < compare:
+        answer = [liquid[start], liquid[end]]
+        compare = abs(compare2)
+    if mid < 0:
         start = mid + 1
-
     else:
         end = mid - 1
 
-print(result)
+print(answer[0], answer[1])
