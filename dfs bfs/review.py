ee63b5798b958c6a9bcff1e5debcc42094c787c5
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-21 13:00:38 +0900

    CodingTestStudy_1021_5

--- dfs bfs/review.py
+++ dfs bfs/review.py
@@ -1,25 +1,54 @@
+import sys
 from collections import deque
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
+input = sys.stdin.readline
 
-def bfs(start_x, start_y):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(a, b, flag, visited):
     queue = deque()
-    queue.append((start_x, start_y))
+    queue.append((a, b))
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-                queue.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
-    print(graph)
-    return graph[N - 1][M - 1]
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0:
+                if flag == 0:
+                    if graph[nx][ny] == graph[x][y]:
+                        queue.append((nx, ny))
+                        visited[nx][ny] = 1
+                else:
+                    if graph[x][y] in ("R", "G"):
+                        if graph[nx][ny] in ("R", "G"):
+                            queue.append((nx, ny))
+                            visited[nx][ny] = 1
+                    else:
+                        if graph[nx][ny] == graph[x][y]:
+                            queue.append((nx, ny))
+                            visited[nx][ny] = 1
 
-N, M = map(int, input().split())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().rstrip())))
 
-print(bfs(0, 0))
+N = int(input())
+graph = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    see = input()
+    for j in range(N):
+        graph[i][j] = see[j]
+
+visited_normal = [[0] * N for _ in range(N)]
+visited_not = [[0] * N for _ in range(N)]
+
+answer = [0, 0]
+for i in range(N):
+    for j in range(N):
+        if visited_normal[i][j] == 0:
+            dfs(i, j, 0, visited_normal)
+            answer[0] += 1
+        if visited_not[i][j] == 0:
+            dfs(i, j, 1, visited_not)
+            answer[1] += 1
+
+print(answer[0], answer[1])
