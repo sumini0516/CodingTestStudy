41d7a642dce7bb2dbe5a4f06010fe6c882b21b9f
Su-Min Lee <72678812+sumini0516@users.noreply.github.com>
2023-10-21 12:36:54 +0900

    CodingTestStudy_1021_3

--- dijkstra, floyd-warshall/review.py
+++ dijkstra, floyd-warshall/review.py
@@ -1,36 +1,43 @@
-import heapq
-from collections import defaultdict
 import sys
-
-def dijkstra(start):
-    queue = []
-    heapq.heappush(queue, (0, start))
-    distance[start] = 0
-    while queue:
-        cost, node = heapq.heappop(queue)
-        if distance[node] < cost:
-            continue
-        for next_cost, next_node in graph[node]:
-            weight = next_cost + cost
-            if weight < graph[next_cost]:
-                distance[next_cost] = weight
-                heapq.heappush(queue, (weight, next_node))
-    return distance
-
+from collections import defaultdict
+import heapq
 
 input = sys.stdin.readline
-
-N = int(input())
-M = int(input())
 INF = int(1e9)
 
+def dijkstra(start, end):
+    distance = [INF] * (N + 1)
+    queue = list()
+    distance[start] = 0
+    heapq.heappush(queue, (start, 0))
+    while queue:
+        node, cost = heapq.heappop(queue)
+        if distance[node] < cost:
+            continue
+        for next_node, next_cost in graph[node]:
+            if cost + next_cost < distance[next_node]:
+                distance[next_node] = cost + next_cost
+                heapq.heappush(queue, (next_node, cost + next_cost))
+    # print(distance)
+    return distance[end]
+
+N, E = map(int, input().split(" "))
 graph = defaultdict(list)
-distance = [INF] * (N + 1)
 
-for i in range(M):
-    u, w, v = map(int, input().split())
-    graph[u].append((w, v))
+for i in range(E):
+    a, b, c = map(int, input().split(" "))
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
-start, end = map(int, input().split())
-result = dijkstra(start)
-print(result[end])
+V1, V2 = map(int, input().split(" "))
+
+path1 = dijkstra(1, V1) + dijkstra(V1, V2) + dijkstra(V2, N)
+path2 = dijkstra(1, V2) + dijkstra(V1, V2) + dijkstra(V1, N)
+
+# print(path1)
+# print(path2)
+
+if path1 >= INF and path2 >= INF:
+    print("-1")
+else:
+    print(min(path1, path2))
